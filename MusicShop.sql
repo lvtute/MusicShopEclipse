1686d2f98a366400ba2ddfe9191b6966d31b854d
Hien Hoang <bluntz.blakz@gmail.com>
2019-06-14 21:05:34 +0700

    solve vulnabilities 2

--- MusicShop.sql
+++ MusicShop.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `musicshop` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `musicshop`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: musicshop
@@ -304,4 +306,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-14 14:14:22
+-- Dump completed on 2019-06-14 21:04:42
--- src/filters/HeaderSettingFilter.java
+++ src/filters/HeaderSettingFilter.java
@@ -1,5 +1,37 @@
 package filters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class HeaderSettingFilter {
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+@WebFilter(filterName = "HeaderSettingFilter", urlPatterns = { "/*" })
+public class HeaderSettingFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(HeaderSettingFilter.class);
+  
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        log.info("All headers added to response");
+        
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("X-XSS-Protection", "1; mode=block");
+        response.setHeader("X-Frame-Options", "deny");
+        response.setHeader("X-Content-Type-Options", "nosniff");
+
+        System.out.println("All headers added to response");
+        
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
 }
