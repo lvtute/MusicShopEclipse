4535281e2039f634901d9519cfec288931dc1810
Hien Hoang <bluntz.blakz@gmail.com>
2019-06-14 20:36:08 +0700

    solve vulnabilities
    - Adding HeaderSettingFilter

--- src/utils/MyUtil.java
+++ src/utils/MyUtil.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.Scanner;
+
 import org.owasp.esapi.ESAPI;
 
 public class MyUtil {
@@ -7,5 +9,43 @@ public class MyUtil {
   		String value = "";
   		value = (String) ESAPI.encoder().encodeForHTML(input);
   		return value;
+	}
+	public static boolean isInteger(String s) {
+   		int radix = 10;
+		Scanner sc = new Scanner(s.trim());
+    		if(!sc.hasNextInt(radix)){
+    			sc.close();
+    			return false;
+    		}
+   		//Chắc chắn ko còn dữ liệu
+   		sc.nextInt(radix);
+    		boolean flag = !sc.hasNext();
+    		sc.close();
+    		return flag;
+	}
+	public static boolean isLong(String s) {
+   		int radix = 10;
+		Scanner sc = new Scanner(s.trim());
+    		if(!sc.hasNextLong(radix)){
+    			sc.close();
+    			return false;
+    		}
+   		//Chắc chắn ko còn dữ liệu
+   		sc.nextLong(radix);
+    		boolean flag = !sc.hasNext();
+    		sc.close();
+    		return flag;
+	}
+	public static boolean isDouble(String s) {
+		Scanner sc = new Scanner(s.trim());
+    		if(!sc.hasNextDouble()){
+    			sc.close();
+    			return false;
+    		}
+   		//Chắc chắn ko còn dữ liệu
+   		sc.nextDouble();
+    		boolean flag = !sc.hasNext();
+    		sc.close();
+    		return flag;
 	}
 }
